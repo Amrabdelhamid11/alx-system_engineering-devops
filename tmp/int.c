5c0164fcd6d21cf48fe2ad2416a4cea990895ab0
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-22 19:56:23 -0700

    dd

---
+++ tmp/int.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	int age=555;
+	printf("the number of byte is %lu\n",sizeof(age));
+	return 0;
+}
---
+++ tmp/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	int age=25;
+	printf("my age is %d\n",age);
+	return 0;
+}
